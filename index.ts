0ecf320284c65804029258dd8043dec674b29472
Yashraj Chouhan <yashrajchouhan14@gmail.com>
2026-02-26 12:07:48 +0530

    add command

---
+++ index.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const {Command} = require("commander")
+const program = new Command()
+
+program
+.command("add <n1> <n2>")
+.action((n1,n2)=>{
+    console.log(Number(n1)+Number(n2))
+})
+program.parse()
